e78f3305e25c3a8d678b5ab39e12ba459adb2b65
Shevonne Polastre <90461625+shevonnepolastre@users.noreply.github.com>
2025-03-30 14:54:59 -0400

    Update autostart-vm.ps1

--- scripts/autostart-vm.ps1
+++ scripts/autostart-vm.ps1
@@ -6,10 +6,10 @@ Param(
 Disable-AzContextAutosave -Scope Process
 
 # Connect using User-assigned Managed Identity
-$clientId = "caf18728-b23d-47d8-936f-abbe6bc7056e"
+$clientId = "[client ID]"
 Connect-AzAccount -Identity -AccountId $clientId
 
-$subscriptionId = "3d36185b-63ee-4012-b04c-98a78f4bf5d7"
+$subscriptionId = "[subscription ID]"
 $AzureContext = Set-AzContext -SubscriptionId $subscriptionId
 
 Start-AzVM -ResourceGroupName $resourceGroup -Name $vmName -DefaultProfile $AzureContext
